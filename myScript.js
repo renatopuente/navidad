0662e5744068d4d3e1247d833b2f029b04b6083a
Renato Puente <66146913+renatopuente@users.noreply.github.com>
2023-12-20 14:22:57 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -28,7 +28,7 @@ let select = s => document.querySelector(s),
 		woodColor = '#A63C06',
 		treeColor = '#0F7173',
     subm1Str = 'MUEVE TU MOUSE Y',
-    subm2Str = 'ADORNA EL ÁRBOL 🎄,
+    subm2Str = 'ADORNA EL ÁRBOL 🎄',
 		followerVX = 0,
 		followerVY = 0
     
